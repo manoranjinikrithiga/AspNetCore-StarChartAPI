c04bf215b693bc91734aa4a362535ec2c499f9a9
manoranjinikrithiga <62386379+manoranjinikrithiga@users.noreply.github.com>
2020-03-31 11:15:44 +0530

    Step1

--- StarChart/Startup.cs
+++ StarChart/Startup.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using StarChart.Data;
 
 namespace StarChart
 {
